1d5e7c464defe93cee7d5d4742f1a9171b31142a
andersonkaiti <anderkaiti@gmail.com>
2025-09-21 18:08:56 -0300

    :sparkles: feat(server): add error handling middleware to log errors and respond with 500 status

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ app.use(express.json())
 
 app.use(router)
 
+app.use((error, _request, response, _next) => {
+  console.log('##### Error handler')
+  console.log(error)
+  response.sendStatus(500)
+})
+
 app.listen(3000, () =>
   console.log('🚀 Server started at http://localhost:3000')
 )
