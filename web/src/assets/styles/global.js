548cca720ee93b5b24d52582e7c9670889ce7d91
andersonkaiti <anderkaiti@gmail.com>
2026-01-05 00:02:07 -0300

    :sparkles: feat(theme): update default theme structure to include color palette for background and primary colors

--- web/src/assets/styles/global.js
+++ web/src/assets/styles/global.js
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-      background-color: ${theme.backgroundColor};
+      background-color: ${theme.colors.background};
       font-size: 16px;
     }
 
