ca7673cadedccc2b9cad9521fe2ad62d55fcb7e1
andersonkaiti <anderkaiti@gmail.com>
2026-01-05 16:37:17 -0300

    :sparkles: feat(contacts,card): add user info card to ContactsList component

--- web/src/components/ContactsList/index.jsx
+++ web/src/components/ContactsList/index.jsx
@@ -1,4 +1,7 @@
-import { Container, Header } from './styles'
+import arrow from '../../assets/images/icons/arrow.svg'
+import edit from '../../assets/images/icons/edit.svg'
+import trash from '../../assets/images/icons/trash.svg'
+import { Card, Container, Header, ListContainer } from './styles'
 
 export default function ContactsList() {
   return (
@@ -8,6 +11,39 @@ export default function ContactsList() {
 
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+      </ListContainer>
+
+      {Array.from({ length: 4 }).map((_, index) => (
+        <Card key={index}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Anderson Kaiti</strong>
+              <small>Instagram</small>
+            </div>
+
+            <span>anderkaiti@gmail.com</span>
+            <span>(14) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   )
 }
--- web/src/components/ContactsList/styles.js
+++ web/src/components/ContactsList/styles.js
@@ -30,3 +30,78 @@ export const Header = styled.header`
     }
   `}
 `
+
+export const ListContainer = styled.div`
+  ${({ theme }) => css`
+    margin-top: 24px;
+
+    header {
+      margin-bottom: 8px;
+
+      button {
+        background: transparent;
+        border: none;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+
+        span {
+          margin-right: 8px;
+          font-weight: bold;
+          color: ${theme.colors.primary.main}
+        }
+      }
+    }
+  `}
+`
+
+export const Card = styled.div`
+  ${({ theme }) => css`
+    background: #fff;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);;
+    padding: 16px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & ~ & {
+      margin-top: 16px;
+    }
+
+    .info {
+      .contact-name {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        
+        small {
+          background: ${theme.colors.primary.lighter};
+          color: ${theme.colors.primary.main};
+          font-weight: bold;
+          text-transform: uppercase;
+          padding: 4px;
+          border-radius: 4px;
+        }
+      }
+
+      span {
+        display: block;
+        font-size: 14px;
+        color: ${theme.colors.gray[200]};
+      }
+    }
+
+    .actions {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      button {
+        background: transparent;
+        border: none;
+        cursor: pointer;
+      }
+    }
+  `}
+`
