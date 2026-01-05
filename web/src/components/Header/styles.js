be62d87e72cc07be727fb346c86842e2e61c30d4
andersonkaiti <anderkaiti@gmail.com>
2026-01-05 18:17:04 -0300

    :sparkles: feat(home): consolidate ContactsList and search functionality into Home component, removing ContactsList component and updating Header styles

--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -1,30 +1,9 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Container = styled.header`
   margin-top: 74px;
+  margin-bottom: 48px;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
-
-export const InputSearchContainer = styled.div`
-  ${({ theme }) => css`
-    margin-top: 48px;
-    width: 100%;
-
-    input {
-      width: 100%;
-      background: #fff;
-      border: none;
-      border-radius: 25px;
-      height: 50px;
-      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
-      outline: none;
-      padding: 0 16px;
-
-      &::placeholder{
-        color: ${theme.colors.gray[200]};
-      }
-    }
-  `}
 `
--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -1,7 +1,26 @@
 import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
-  margin-top: 32px;
+export const Container = styled.div``
+
+export const InputSearchContainer = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+
+    input {
+      width: 100%;
+      background: #fff;
+      border: none;
+      border-radius: 25px;
+      height: 50px;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+      outline: none;
+      padding: 0 16px;
+
+      &::placeholder{
+        color: ${theme.colors.gray[200]};
+      }
+    }
+  `}
 `
 
 export const Header = styled.header`
@@ -9,6 +28,7 @@ export const Header = styled.header`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    margin-top: 32px;
 
     strong {
       color: #222;
