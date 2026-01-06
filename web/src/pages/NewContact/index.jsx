01a5ce8d2eb7e1dcc028505973ec30bafcfa6997
andersonkaiti <anderkaiti@gmail.com>
2026-01-05 21:46:23 -0300

    :sparkles: feat(components): add Input and Select components with styled-components for consistent styling

--- web/src/pages/NewContact/index.jsx
+++ web/src/pages/NewContact/index.jsx
@@ -1,5 +1,16 @@
+import { Input } from '../../components/Input'
 import { PageHeader } from '../../components/PageHeader'
+import { Select } from '../../components/Select'
 
 export function NewContact() {
-  return <PageHeader title="Novo contato" />
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="">Selecione</option>
+      </Select>
+    </>
+  )
 }
