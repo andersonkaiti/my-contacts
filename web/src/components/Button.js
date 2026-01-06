2e5e04f8f47906fe001139489b3801fbad876649
andersonkaiti <anderkaiti@gmail.com>
2026-01-05 21:58:30 -0300

    :sparkles: feat(components): add Button component with styled-components

---
+++ web/src/components/Button.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components'
+
+export const Button = styled.button`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 52px;
+    border: none;
+    background: ${theme.colors.primary.main};
+    font-size: 16px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    font-weight: bold;
+    color: #fff;
+    border-radius: 4px;
+    transition: background .2s ease-in;
+
+    &:hover {
+      background: ${theme.colors.primary.light};
+    }
+
+    &:active {
+      background: ${theme.colors.primary.dark};
+    }
+
+    &:disabled {
+      background: #ccc;
+      cursor: default;
+    }
+  `}
+`
