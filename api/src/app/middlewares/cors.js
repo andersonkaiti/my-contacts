4a6fdb68bb05faac96259c4a88256f488098d8af
andersonkaiti <anderkaiti@gmail.com>
2026-01-10 17:11:17 -0300

    :sparkles: feat(middlewares): enhance CORS middleware with additional headers and max age setting

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,5 +1,13 @@
+const ONE_SECOND = 60
+const ONE_MINUTE = ONE_SECOND * 60
+const ONE_HOUR = ONE_MINUTE * 60
+const TWO_HOURS = ONE_HOUR * 2
+
 export function corsMiddleware(_request, response, next) {
   response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', '*')
+  response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader('Access-Control-Max-Age', TWO_HOURS)
 
   next()
 }
