e0c702417c8ed2b8f76c2c9d9659f09011132577
andersonkaiti <anderkaiti@gmail.com>
2026-01-10 18:35:03 -0300

    :sparkles: feat(home): fetch and display contacts with dynamic count and formatting

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import arrow from '../../assets/images/icons/arrow.svg'
 import edit from '../../assets/images/icons/edit.svg'
 import trash from '../../assets/images/icons/trash.svg'
+import { formatPhone } from '../../utils/formatPhone'
 import {
   Card,
   Container,
@@ -11,6 +13,17 @@ import {
 } from './styles'
 
 export function Home() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const data = await response.json()
+        setContacts(data)
+      })
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <Container>
       <InputSearchContainer>
@@ -18,7 +31,9 @@ export function Home() {
       </InputSearchContainer>
 
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length} {contacts.length === 1 ? 'contato' : 'contatos'}
+        </strong>
 
         <Link to="/new">Novo contato</Link>
       </Header>
@@ -32,20 +47,22 @@ export function Home() {
         </header>
       </ListContainer>
 
-      {Array.from({ length: 4 }).map((_, index) => (
-        <Card key={index}>
+      {/* Quando um array é adicionado, o React entende que deve renderizar cada item do array. Por conta disso, a transformação de um array de objetos em um array de componentes JSX é feita: */}
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
-              <strong>Anderson Kaiti</strong>
-              <small>Instagram</small>
+              <strong>{contact.name}</strong>
+
+              {contact.category_id && <small>{contact.category_name}</small>}
             </div>
 
-            <span>anderkaiti@gmail.com</span>
-            <span>(14) 99999-9999</span>
+            <span>{contact.email}</span>
+            <span>{formatPhone(contact.phone)}</span>
           </div>
 
           <div className="actions">
-            <Link to="/edit/123">
+            <Link to={`/edit/${contact.id}`}>
               <img src={edit} alt="Edit" />
             </Link>
 
