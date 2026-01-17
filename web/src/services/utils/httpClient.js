93522bfa1159fa45a8abefc9b20224044f82b232
andersonkaiti <anderkaiti@gmail.com>
2026-01-16 23:24:06 -0300

    :recycle: refactor(httpClient): improve response handling by returning parsed JSON only when applicable and enhancing error messages

--- web/src/services/utils/httpClient.js
+++ web/src/services/utils/httpClient.js
@@ -12,24 +12,24 @@ class HttpClient {
       url.searchParams.set(key, value)
     }
 
-    // Os códigos de status HTTP são retornados como resposta e servem para identificar se a requisição foi bem-sucedida ou não
-
-    // Um erro pode ser causado por:
-    // - Tentar acessar um servidor que não existe
-    // - Erro de CORS
-    // - Caso um endpoint 404 retorne um HTML em vez de JSON
-
-    // O papel da fetch API é apenas fazer a requisição, e não rejeitar promises
-
     const response = await fetch(url)
 
-    // A propriedade ok verifica se o código de status HTTP está entre 200 e 299
-    if (!response.ok) {
-      // Quando um erro é lançado, ele vai descendo a Call Stack até encontrar o bloco catch
-      throw new Error(`${response.status} - ${response.statusText}`)
+    let data = null
+
+    // Headers são case insensitive
+    const contentType = response.headers.get('content-type')
+
+    if (contentType.includes('application/json')) {
+      data = await response.json()
     }
 
-    return response.json()
+    if (!response.ok) {
+      throw new Error(
+        data?.error || `${response.status} ${response.statusText}`,
+      )
+    }
+
+    return data
   }
 }
 
