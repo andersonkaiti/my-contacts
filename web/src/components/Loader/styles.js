dc524924299b6f6b9f72e59d893566aa52b0dba4
andersonkaiti <anderkaiti@gmail.com>
2026-01-18 21:47:25 -0300

    :lipstick: fix(loader): change loader position from absolute to fixed

--- web/src/components/Loader/styles.js
+++ web/src/components/Loader/styles.js
@@ -38,7 +38,7 @@ export const Overlay = styled.div`
   width: 100%;
   height: 100%;
   inset: 0;
-  position: absolute;
+  position: fixed;
   background-color: rgba(246, 245, 252, 0.8);
   display: flex;
   justify-content: center;
