31e30b7bc999fb225a81e68ce20f4b4fab8a356f
andersonkaiti <anderkaiti@gmail.com>
2026-01-18 21:51:02 -0300

    :sparkles: feat(home,cta): add refetch handler to cta button

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -6,7 +6,6 @@ import trash from '../../assets/images/icons/trash.svg'
 import sad from '../../assets/images/sad.svg'
 import { Button } from '../../components/Button'
 import { Loader } from '../../components/Loader'
-import { APIError } from '../../errors/apiError'
 import { contactService } from '../../services/contactsService'
 import { formatPhone } from '../../utils/formatPhone'
 import {
@@ -33,20 +32,19 @@ export function Home() {
     [contacts, searchTerm],
   )
 
-  useEffect(() => {
-    async function loadContacts() {
-      startTransition(async () => {
-        try {
-          const data = await contactService.listContacts(orderBy)
-          setContacts(data)
-        } catch (error) {
-          if (error instanceof APIError) {
-            setHasError(true)
-          }
-        }
-      })
-    }
+  async function loadContacts() {
+    startTransition(async () => {
+      try {
+        const data = await contactService.listContacts(orderBy)
+        setContacts(data)
+        setHasError(false)
+      } catch {
+        setHasError(true)
+      }
+    })
+  }
 
+  useEffect(() => {
     loadContacts()
   }, [orderBy])
 
@@ -56,6 +54,10 @@ export function Home() {
 
   function handleSearchTermChange(event) {
     setSearchTerm(event.target.value)
+  }
+
+  function handleTryAgain() {
+    loadContacts()
   }
 
   return (
@@ -87,44 +89,53 @@ export function Home() {
           <img src={sad} alt="Sad" />
           <div className="details">
             <strong>Ocorreu um erro ao obter os seus contatos!</strong>
-            <Button type="button">Tentar novamente</Button>
+
+            <Button type="button" onClick={handleTryAgain}>
+              Tentar novamente
+            </Button>
           </div>
         </ErrorContainer>
       )}
 
-      {filteredContacts.length > 0 && (
-        <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToggleOrderBy}>
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        </ListHeader>
+      {!hasError && (
+        <>
+          {filteredContacts.length > 0 && (
+            <ListHeader orderBy={orderBy}>
+              <button type="button" onClick={handleToggleOrderBy}>
+                <span>Nome</span>
+                <img src={arrow} alt="Arrow" />
+              </button>
+            </ListHeader>
+          )}
+
+          {filteredContacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+
+                  {contact.category_id && (
+                    <small>{contact.category_name}</small>
+                  )}
+                </div>
+
+                <span>{contact.email}</span>
+                <span>{formatPhone(contact.phone)}</span>
+              </div>
+
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Edit" />
+                </Link>
+
+                <button type="button">
+                  <img src={trash} alt="Delete" />
+                </button>
+              </div>
+            </Card>
+          ))}
+        </>
       )}
-
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-
-              {contact.category_id && <small>{contact.category_name}</small>}
-            </div>
-
-            <span>{contact.email}</span>
-            <span>{formatPhone(contact.phone)}</span>
-          </div>
-
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-      ))}
     </Container>
   )
 }
