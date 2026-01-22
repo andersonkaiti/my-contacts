3eb9b9f1542c8eea9152e837911c665bcb17257b
andersonkaiti <anderkaiti@gmail.com>
2026-01-21 22:57:10 -0300

    :sparkles: feat(httpClient): add post method

--- web/src/services/utils/httpClient.js
+++ web/src/services/utils/httpClient.js
@@ -30,6 +30,39 @@ class HttpClient {
 
     return data
   }
+
+  async post({ path, body }) {
+    const url = new URL(this.baseURL)
+
+    url.pathname = path
+
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    })
+
+    // O HTTP, Hypertext Transfer Protocol, é um protocolo de transferência de
+    // hipertexto, então não é possível enviar um objeto JavaScript, mas sim
+    // uma string JSON.
+    const response = await fetch(url, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body),
+    })
+
+    let data = null
+
+    const contentType = response.headers.get('content-type')
+
+    if (contentType.includes('application/json')) {
+      data = await response.json()
+    }
+
+    if (!response.ok) {
+      throw new APIError(response, data)
+    }
+
+    return data
+  }
 }
 
 export { HttpClient }
