781327041601461160bb0987350e89e3bc502c80
andersonkaiti <anderkaiti@gmail.com>
2026-01-25 17:03:07 -0300

    :sparkles: feat(toastMessage): improve accessibility with tabIndex and role props

--- web/src/components/Toast/ToastMessage/index.jsx
+++ web/src/components/Toast/ToastMessage/index.jsx
@@ -8,7 +8,14 @@ export function ToastMessage({ message: { id, text, type }, onRemoveMessage }) {
   }
 
   return (
-    <Container type={type} onClick={handleRemoveToast}>
+    <Container
+      type={type}
+      onClick={handleRemoveToast}
+      // A posição do elemento ao navegar com tab é 0 (primeira posição)
+      tabIndex={0}
+      // Define o elemento como um botão
+      role="button"
+    >
       {type === 'danger' && <img src={xCircleIcon} alt="X" />}
       {type === 'success' && <img src={checkCircleIcon} alt="Check" />}
       <strong>{text}</strong>
