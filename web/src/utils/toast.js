31f07caa32a3861e33ac79569e95cb00fc420410
andersonkaiti <anderkaiti@gmail.com>
2026-01-25 14:16:10 -0300

    :sparkles: feat(eventManager): implement custom event manager from scratch

--- web/src/utils/toast.js
+++ web/src/utils/toast.js
@@ -1,7 +1,4 @@
 export function toast({ type = 'success', text = '' }) {
-  // O nome do evento é case sensitive
-  // new Event -> para criar um evento sem enviar dados
-  // new CustomEvent -> para criar um evento com dados
   const event = new CustomEvent('addtoast', {
     detail: {
       type,
